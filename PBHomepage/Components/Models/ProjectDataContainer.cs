f483fd07ca7700bae9f112f0e978d164bb8e9911
BauerPhilipp <fam.bauer@live.at>
2025-01-24 18:01:12 +0100

    Implement Blazor DI Container object

---
+++ PBHomepage/Components/Models/ProjectDataContainer.cs
@@ -0,0 +1,24 @@
+﻿using PBHomepage.Components.Pages.ProjectPages.Print;
+
+namespace PBHomepage.Components.Models
+{
+    public class ProjectDataContainer
+    {
+        public List<CardData> CardsData = new List<CardData>()
+        {
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand"),
+            new CardData(ImageLink:"Projects/Printing/WacomStand/Thumbnail.jpg", ImageText:"WacomStand", PageLink:"/PrintWacomStand")
+        };
+
+    }
+
+    public record CardData(string ImageLink, string ImageText, string PageLink);
+}
--- PBHomepage/Program.cs
+++ PBHomepage/Program.cs
@@ -1,10 +1,13 @@
 using PBHomepage.Components;
+using PBHomepage.Components.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<ProjectDataContainer>();
 
 var app = builder.Build();
 
